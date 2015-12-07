c68c9111420ad22720fb00db8b3ecb172f2db3f2
Tolga Tiryaki <tolga@tolgamac.local>
2015-12-07 19:46:42 +0200

    added files for supercool

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Tolga Tiryaki on 05/12/15.
+//  Copyright © 2015 Tolga Tiryaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var coolLogo: UIImageView!
+    @IBOutlet weak var coolBg: UIImageView!
+    @IBOutlet weak var uncoolButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func makeMeNotSoUncool(sender: AnyObject) {
+        coolLogo.hidden = false
+        coolBg.hidden = false
+        uncoolButton.hidden = true
+        
+        
+    }
+
+}
+
